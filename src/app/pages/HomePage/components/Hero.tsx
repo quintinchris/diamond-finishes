19c577a334d72404e8d42d7fd386f2f08ccd568f
QuintinChris <quintinchrisdev@gmail.com>
2021-12-29 21:13:12 -0500

    added images to hero, added second hero option

--- src/app/pages/HomePage/components/Hero.tsx
+++ src/app/pages/HomePage/components/Hero.tsx
@@ -2,41 +2,27 @@
 /* eslint-disable jsx-a11y/alt-text */
 export function Hero() {
   return (
-    <div className="bg-white dark:bg-gray-800 flex relative z-20 items-center overflow-hidden">
-      <div className="container mx-auto px-6 flex relative py-16">
-        <div className="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
-          <span className="w-20 h-2 bg-gray-800 dark:bg-white mb-12"></span>
-          <h1 className="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
-            Be on
-            <span className="text-5xl sm:text-7xl">Time</span>
-          </h1>
-          <p className="text-sm sm:text-base text-gray-700 dark:text-white">
-            Dimension of reality that makes change possible and understandable.
-            An indefinite and homogeneous environment in which natural events
-            and human existence take place.
-          </p>
-          <div className="flex mt-8">
-            <a
-              href="#"
-              className="uppercase py-2 px-4 rounded-lg bg-pink-500 border-2 border-transparent text-white text-md mr-4 hover:bg-pink-400"
+    <div className="bg-white dark:bg-gray-800 overflow-hidden relative">
+      <div className="text-start w-1/2 py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+        <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+          <span className="block">Want to be millionaire ?</span>
+          <span className="block text-indigo-500">It&#x27;s today or never.</span>
+        </h2>
+        <div className="lg:mt-0 lg:flex-shrink-0">
+          <div className="mt-12 inline-flex rounded-md shadow">
+            <button
+              type="button"
+              className="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
             >
               Get started
-            </a>
-            <a
-              href="#"
-              className="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-pink-500 text-pink-500 dark:text-white hover:bg-pink-500 hover:text-white text-md"
-            >
-              Read more
-            </a>
+            </button>
           </div>
         </div>
-        <div className="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
-          <img
-            src="/images/object/10.png"
-            className="max-w-xs md:max-w-sm m-auto"
-          />
-        </div>
       </div>
+      <img
+        src="/images/IMG-0682.jpg"
+        className="absolute h-full max-w-1/2 hidden lg:block right-8 top-0"
+      />
     </div>
   );
 }
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,9 +1,10 @@
 import { Hero } from './components/Hero';
+import { Hero2 } from './components/Hero2';
 
 export function HomePage() {
     return (
         <>
-        <Hero />
+        <Hero2 />
         </>
     )
 }
