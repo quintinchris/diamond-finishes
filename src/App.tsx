b401dc3ed46c2d5cb95ce9c8107d76744874341a
QuintinChris <quintinchrisdev@gmail.com>
2021-12-30 08:51:47 -0500

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { HomePage } from "./app/pages/HomePage";
 import { NavBar } from "./app/components/NavBar";
+import { Footer } from "./app/components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <HomePage />
+      <Footer />
     </>
   );
 }
