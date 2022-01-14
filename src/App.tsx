546536172f680a7aa88e6b6e30fa6d373c6f831d
QuintinChris <quintinchrisdev@gmail.com>
2022-01-13 23:32:57 -0500

    integrating helmet and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import logo from "./logo.svg";
+import { Helmet } from "react-helmet-async";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";import { useTranslation } from 'react-i18next';
+
 import "./App.css";
 import { HomePage } from "./app/pages/HomePage";
-import { NavBar } from "./app/components/NavBar/NavBar";
-import { Footer } from "./app/components/Footer/Footer";
 
 function App() {
+  const { i18n } = useTranslation();
   return (
-    <>
-      <NavBar />
-      <HomePage />
-      <Footer />
-    </>
+    <Router>
+      <Helmet
+        titleTemplate="%s - Diamond Finishes LLC"
+        defaultTitle="Diamond Finishes LLC"
+        htmlAttributes={{ lang: i18n.language }}
+      >
+        <meta name="description" content="Quality Carpentry Services" />
+      </Helmet>
+
+      <Routes>
+        <Route path={process.env.PUBLIC_URL + "/"} element={<HomePage />} />
+        {/* TODO: ADD ROUTES TO PORTFOLIO AND CONTACT PAGES */}
+      </Routes>
+      {/* <GlobalStyle /> */}
+    </Router>
   );
 }
 
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -6,16 +6,20 @@ import { Features } from './components/Features';
 import { Team } from './components/Team';
 import { Testimonials } from './components/Testimonials';
 import { Form } from '../../components/Form/Form';
+import { NavBar } from "../../components/NavBar/NavBar";
+import { Footer } from "../../components/Footer/Footer";
 
 
 export function HomePage() {
     return (
         <>
+        <NavBar />
         <Hero4 />
         <Features />
         <Team />
         <Testimonials />
         <Form />
+        <Footer />
         </>
     )
 }
