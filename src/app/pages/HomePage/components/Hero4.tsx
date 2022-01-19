08967ea27c255632f9f07b4a30b56453e6b44a43
QuintinChris <quintinchrisdev@gmail.com>
2022-01-19 08:08:57 -0500

    fixed landing page

--- src/app/pages/HomePage/components/Hero4.tsx
+++ src/app/pages/HomePage/components/Hero4.tsx
@@ -3,33 +3,39 @@ import { scrollToElement } from "../../../utils/scrollToElement";
 
 export function Hero4() {
   return (
-    // gradient background
-    // <div className="bg-gradient-to-r bg-blur-sm from-maroon/90 via-maroon to-maroon/90 dark:bg-gray-800 overflow-hidden h-screen flex">
-    <div className="bg-maroon dark:bg-gray-800 relative overflow-hidden h-screen z-0">
-      <img className="absolute place-self-center max-h-screen w-full" src="/icons/diamond-3.svg" alt="Workflow" />
+    <div className="bg-maroon dark:bg-gray-800 relative overflow-hidden h-full z-0">
+      <div className="flex flex-col content-center justify-center align-center">
+        <img
+          className="place-self-center w-3/5 h-1/4 mt-4 mb-6"
+          src="/icons/diamond-outline.svg"
+          alt="Diamond"
+        />
+      </div>
       <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center pt-24 pl-4 w-2/5">
-        <h2 className="font-['Poppins'] font-semibold text-white dark:text-white sm:text-4xl">
-          <span className="block text-6xl leading-tight">
-            Bring your ideas to life
-            {/* Make Your Home Shine */}
-          </span>
-          {/* <span className="block text-3xl pt-2">
+        <div className="flex flex-col justify-items-center">
+          <h2 className="font-['Poppins'] font-semibold text-white dark:text-white sm:text-4xl">
+            <span className="block text-6xl leading-tight place-self-center">
+              Bring your ideas to life
+              {/* Make Your Home Shine */}
+            </span>
+            {/* <span className="block text-3xl pt-2">
             With Diamond Finishes
           </span> */}
-        </h2>
-        <p className="text-md mt-4 text-slate-200">
-          Providing Quality Carpentry Services In &amp; Around Historic New
-          Castle, DE.
-        </p>
-          <div className="mt-12 inline-flex rounded-md shadow">
+          </h2>
+          <p className="text-md text-center place-self-center mt-6 w-3/4 text-slate-300">
+            Providing Quality Carpentry Services In &amp; Around Historic New
+            Castle, DE.
+          </p>
+          <div className="mt-10 -ml-2">
             <button
               type="button"
-              onClick={() => scrollToElement("#contactus")}
-              className="py-2 px-4 bg-gray-600 hover:bg-gray-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              onClick={() => scrollToElement(".contact")}
+              className="py-2 px-4 w-1/3 bg-gray-600 hover:bg-gray-800 focus:ring-green-500 focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
             >
-              Contact Us
+              <a href="/contact">Get A Quote!</a>
             </button>
           </div>
+        </div>
       </div>
     </div>
   );
