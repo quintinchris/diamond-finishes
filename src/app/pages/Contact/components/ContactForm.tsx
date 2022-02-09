2c0b8cbe50bea779a8b015ecc3416bc69dc558b4
QuintinChris <quintinchrisdev@gmail.com>
2022-02-09 08:06:16 -0500

    fixed contact page mobile styling

--- src/app/pages/Contact/components/ContactForm.tsx
+++ src/app/pages/Contact/components/ContactForm.tsx
@@ -82,41 +82,43 @@ export function ContactForm() {
 
   return (
     <div className="bg-maroon text-gray-100 px-8 py-12 overflow-hidden">
-      <div className="max-w-screen-xl px-8 pr-20 grid gap-8 grid-cols-1 md:grid-cols-2 py-2 mx-auto text-gray-900">
-        <div className="flex flex-col gap-8 mr-2 justify-between">
+      <div className="max-w-screen-xl md:px-8 md:pr-20 grid gap-8 grid-cols-1 md:grid-cols-2 py-2 mx-auto text-gray-900">
+        <div className="grid grid-cols-1 gap-8 mr-2 justify-between text-center md:text-left">
           <div>
             <h2 className="text-4xl lg:text-5xl text-white font-['Poppins'] font-bold leading-tight">
               Get a quote
             </h2>
-            <div className="text-gray-200 mt-6 w-5/6">
+            <div className="text-gray-200 mt-6 md:w-5/6">
               Fill out the form and our team will get back to you as soon as
               possible!
             </div>
-            <div className="text-center text-white mt-16">
-              <div className="md:[flex flex-row]">
-                <div className="hover:box-border px-4 py-6 w-3/4 text-center rounded-lg hover:bg-maroon3 hover:border-2 hover:border-white cursor-pointer">
-                  <a
-                    className="flex flex-row items-center content-center justify-start"
-                    href="tel:302-279-6114"
-                  >
-                    <BsTelephoneFill color="white" className="w-10 h-10 pr-4"/>
-                    <span>(302) 279-6114</span>
-                  </a>
-                </div>
-                <div className="hover:box-border mt-2 px-4 py-6 w-3/4 text-center rounded-lg hover:border-2 hover:border-white hover:bg-maroon3 cursor-pointer">
-                  <a
-                    className="flex flex-row items-center content-center justify-start"
-                    href="mailto: DiamondFinishes2@gmail.com?subject=NewInquiryFromWebsite"
-                  >
-                    <FaRegEnvelope color="white" className="w-10 h-10 pr-4" />
-                    <span>diamondfinishes2@gmail.com</span>
-                  </a>
-                </div>
+            <div className="grid grid-cols-2 gap-6 md:grid-cols-1 text-center text-white mt-8 md:mt-16">
+              <div className="hover:box-border px-4 py-6 md:w-3/4 text-center rounded-lg hover:bg-maroon3 hover:border-2 hover:border-white cursor-pointer">
+                <a
+                  className="flex flex-row items-center content-center justify-start"
+                  href="tel:302-279-6114"
+                >
+                  <BsTelephoneFill color="white" className="w-10 h-10 pr-4" />
+                  <span className="hidden md:block">(302) 279-6114</span>
+                  <span className="block md:hidden">Call us!</span>
+                </a>
+              </div>
+              <div className="hover:box-border md:mt-2 px-4 py-6 md:w-3/4 text-center rounded-lg hover:border-2 hover:border-white hover:bg-maroon3 cursor-pointer">
+                <a
+                  className="flex flex-row items-center content-center justify-start"
+                  href="mailto: DiamondFinishes2@gmail.com?subject=NewInquiryFromWebsite"
+                >
+                  <FaRegEnvelope color="white" className="w-10 h-10 pr-4" />
+                  <span className="hidden md:block">
+                    diamondfinishes2@gmail.com
+                  </span>
+                  <span className="block md:hidden">Email us!</span>
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="ml-6 rounded-lg bg-gray-200 shadow-lg px-6 py-8 -mt-4 -mr-14">
+        <div className="md:ml-6 rounded-lg bg-gray-200 shadow-lg px-6 py-8 md:-mt-4 md:-mr-14">
           <form onSubmit={handleSubmit}>
             <div>
               <span className="uppercase text-sm text-gray-600 font-bold">
@@ -160,7 +162,10 @@ export function ContactForm() {
                   onChange={handleImageUpload}
                 />
                 <div className="flex flex-row items-center content-center justify-start cursor-pointer z-10">
-                  <MdOutlineUpload color="white" className="w-10 h-10 pr-4 cursor-pointer" />
+                  <MdOutlineUpload
+                    color="white"
+                    className="w-10 h-10 pr-4 cursor-pointer"
+                  />
                   <span className="text-white font-['Poppins'] text-sm cursor-pointer">
                     Upload Image
                   </span>
